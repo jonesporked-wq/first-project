b6d1a1126659d5be537d2ce40b99c603425daedb
jonesporked-wq <jonesporked@gmail.com>
2026-06-23 22:44:01 +0800

    commit message

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -325,7 +325,7 @@ function HomeScreen({ navigation }: any) {
       setLoading(true);
       setError(null);
       
-      const response = await fetch('https://raw.githubusercontent.com/adamalexisyahya-ops/Rainbow-Coffee/main/menu.json');
+      const response = await fetch('https://raw.githubusercontent.com/jonesporked-wq/first-project/blob/main/menu.json');
       
       if (!response.ok) {
         throw new Error('Could not retrieve menu data');
